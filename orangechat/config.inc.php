a065fdea17179bbc0942f51d83eb9b16ce9e7961
Jefrey <jefrey.sobreira@gmail.com>
2014-03-03 20:25:29 -0300

    First working release

--- orangechat/config.inc.php
+++ orangechat/config.inc.php
@@ -1,13 +1,19 @@
 <?php
 
+/* MySQL data */
 define ('DBPATH','localhost');
 define ('DBUSER','root');
 define ('DBPASS','');
 define ('DBNAME','orangechat');
 
+/* Path to orangechat folder (relative to server root) */
 define('ORANGE_BASE', '/orangechat/orangechat/');
 
+/* Language ISO code. It must be in "lang" folder */
 define('LANGUAGE', 'en');
+
+/* Theme name. It must be in "themes" folder. */
 define('THEME', 'orange');
 
-define('TIMEFORMAT', 'd/m/Y H:i'); // see php.net/date
+/* Time format. See php.net/date */
+define('TIMEFORMAT', 'd/m/Y H:i');
